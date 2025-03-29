5c6fad2aff3424ce82070d4d224505ac8d9e8f84
Bhavya Saini <bhavyasaini06@gmail.com>
2025-03-29 14:26:10 +0530

    For Second folder

---
+++ Conditional_statements_and_loops/Even_or_odd.java
@@ -0,0 +1,19 @@
+package Java_Codes.Conditional_statements_and_loops;
+import java.util.Scanner;
+
+public class Even_or_odd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+    
+}
---
+++ Conditional_statements_and_loops/Factorial.java
@@ -0,0 +1,25 @@
+package Java_Codes.Conditional_statements_and_loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args){
+        
+        int num;
+        int fact = 1;
+        System.out.println("Enter a number to find its factorial : ");
+        Scanner obj = new Scanner(System.in);
+        num = obj.nextInt();
+        obj.close();
+
+        for(int i = 1; i <= num; i++){
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + num + " is : " + fact);
+        
+        
+    }
+    
+}
+
