9d4abf22644ce7d8813f4bd45522a6505460351b
Bhavya Saini <bhavyasaini06@gmail.com>
2025-03-29 14:00:44 +0530

    Checks

---
+++ Basic_Syntax/HelloWorld.java
@@ -0,0 +1,15 @@
+package Java_Codes.Basic_Syntax;
+
+public class HelloWorld {
+
+    public static void sayHello(){
+
+        System.out.println("Hello World!");
+
+    }
+
+    public static void main(String[] args){
+
+        sayHello();
+    } 
+}
