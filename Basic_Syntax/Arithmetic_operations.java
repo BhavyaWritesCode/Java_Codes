e33c8a52608a32637b35fd42289592013441e984
Bhavya Saini <bhavyasaini06@gmail.com>
2025-03-29 14:11:01 +0530

    Added Code

---
+++ Basic_Syntax/Arithmetic_operations.java
@@ -0,0 +1,24 @@
+package Java_Codes.Basic_Syntax;
+
+public class Arithmetic_operations {
+
+    public static void operations(){
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Addition : " + (a + b));
+        System.out.println("Subtraction : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division : " + (a / b));
+        System.out.println("Modulus : " + (a % b));
+    }
+
+    public static void main(String[] args){
+
+        operations();
+
+    }
+    
+}
+
---
+++ Basic_Syntax/Swapping.java
@@ -0,0 +1,34 @@
+package Java_Codes.Basic_Syntax;
+
+import java.util.Scanner;
+
+public class Swapping {
+
+    public static void main(String[] args){
+
+        Scanner obj = new Scanner(System.in);
+
+        int a , b;
+
+        System.out.println("Enter the value of a : ");
+        a = obj.nextInt();
+        System.out.println("Enter the value of b : ");
+        b = obj.nextInt();
+
+        System.out.println("Before Swapping :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swapping :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        obj.close();
+
+    }
+    
+}
