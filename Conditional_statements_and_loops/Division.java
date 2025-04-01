60d909ac1aec6d7387a7efe855eebe78cb8538f8
Bhavya Saini <bhavyasaini06@gmail.com>
2025-04-01 21:19:26 +0530

    New Program

---
+++ Conditional_statements_and_loops/Division.java
@@ -0,0 +1,31 @@
+package Java_Codes.Conditional_statements_and_loops;
+import java.util.Scanner;
+
+public class Division {
+
+    public static void main(String[] args){
+
+        System.out.println("Enter the Number till You want Result: ");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){ 
+            if(i % 3 == 0 && i % 5 == 0){ 
+                System.out.println("FIZZBUZZ");
+            }
+            else if(i % 3 == 0){ 
+                System.out.println("FIZZ");
+            }
+            else if(i % 5 == 0){ 
+                System.out.println("BUZZ");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+        System.out.println("End of Program");
+    }
+}
