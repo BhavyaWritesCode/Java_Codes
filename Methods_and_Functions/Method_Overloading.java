9a5207cc497fd1fd803d6032d94ec48cddbb084a
Bhavya Saini <bhavyasaini06@gmail.com>
2025-08-02 23:24:25 +0530

    New file

---
+++ Methods_and_Functions/Method_Overloading.java
@@ -0,0 +1,26 @@
+package Methods_and_Functions;
+
+public class Method_Overloading {
+
+    // Area of square
+    public static int area(int side) {
+        return side * side;
+    }
+
+    // Area of rectangle
+    public static int area(int length, int width) {
+        return length * width;
+    }
+
+    // Area of circle
+    public static double area(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    //Main
+    public static void main(String[] args) {
+        System.out.println("Area of square: " + area(4));
+        System.out.println("Area of rectangle: " + area(5, 7));
+        System.out.println("Area of circle: " + area(3.5));
+    }
+}
